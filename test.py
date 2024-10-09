b0a7c60e1603269334c23996133a283890375bd9
Jeremy Hise <jeremy@hisescientific.com>
2024-10-09 18:33:50 +0100

    Initial Import

---
+++ test.py
@@ -0,0 +1,28 @@
+import serial
+import time
+
+serial_port = "/dev/ttyUSB0"
+baud_rate = 115200
+
+ser = serial.Serial(serial_port, baud_rate)
+time.sleep(2)
+print(ser.read_all().decode('utf-8'))
+		
+ser.write(b'$$\n')
+print(ser.read_all().decode('utf-8'))
+
+command = []
+
+command.append(b'G20\n')
+command.append(b'G90\n')
+command.append(b'G54\n')
+command.append(b'G00 X3.0 Y2.0 Z0.4\n')
+
+
+for c in command:
+	print(c)
+	ser.write(c)
+	time.sleep(2)
+	print(ser.read_all().decode('utf-8'))
+	time.sleep(1)
+
