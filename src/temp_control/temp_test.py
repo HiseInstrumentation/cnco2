d61d0e0daaacfa96cbdf7e51267896ee45edf73e
Jeremy Hise <jeremy@hisescientific.com>
2024-11-14 13:38:24 -0500

    - Python __main__

--- src/temp_control/temp_test.py
+++ src/temp_control/temp_test.py
@@ -1,21 +1,23 @@
 import serial
 import time
 
-# Open up serial to heater
-heater = serial.Serial("/dev/ttyUSB0", 9600)
-heater.reset_input_buffer()
-time.sleep(5)
+if __name__ == '__main__':
 
-# Write something to it
+        # Open up serial to heater
+        heater = serial.Serial("/dev/ttyUSB0", 9600)
+        heater.reset_input_buffer()
+        time.sleep(5)
+
+        # Write something to it
 
 
-# Read something from it
-while True:
-        heater.write(b'testy\n')
-        time.sleep(2)
-        response = heater.readline().decode('utf-8').strip()
-        if(response != ""):
-                print("\nResponse:")
-                print("\n"+response+"\n")
+        # Read something from it
+        while True:
+                heater.write(b'testy\n')
+                time.sleep(2)
+                response = heater.readline().decode('utf-8').strip()
+                if(response != ""):
+                        print("\nResponse:")
+                        print("\n"+response+"\n")
 
 
