43ac3a84f1d17b608f33599e9bae56a59f283941
Jeremy Hise <jeremy@hisescientific.com>
2025-02-18 12:43:55 -0500

    -

--- src/o2readtest.py
+++ src/o2readtest.py
@@ -4,18 +4,22 @@ import re
 
 sensor = serial.Serial("/dev/ttyUSB1", 19200)
 
+time.sleep(2);
+
 sensor.write(b'M\n')
 time.sleep(2)
 ret_str = sensor.read_all().decode('utf-8')
 print(ret_str)
 
-regex_o2 = "^[0-9]*\.[0-9]*"
-regex_te = "[0-9]*\.[0-9]*\B"
-regex_pr = "[0-9]{4}|[0-9]{3}"
+'''
+    regex_o2 = "^[0-9]*\.[0-9]*"
+    regex_te = "[0-9]*\.[0-9]*\B"
+    regex_pr = "[0-9]{4}|[0-9]{3}"
             
 
-o2 = re.search(regex_o2, ret_str).group()
-temp = re.search(regex_te, ret_str).group()
-pressure = re.search(regex_pr, ret_str).group()
+    o2 = re.search(regex_o2, ret_str).group()
+    temp = re.search(regex_te, ret_str).group()
+    pressure = re.search(regex_pr, ret_str).group()
 
-print("Returned: " + o2 + " : " + temp + " : " + pressure )
+    print("Returned: " + o2 + " : " + temp + " : " + pressure )
+'''
--- src/temp_control/temp_test.py
+++ src/temp_control/temp_test.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
 
         # Open up serial to heater
         port = "/dev/ttyUSB0"
-        heater = serial.Serial(port, 115200)
+        heater = serial.Serial(port, 115200, timeout=3)
         heater.reset_input_buffer()
         time.sleep(2)
 
@@ -13,14 +13,15 @@ if __name__ == '__main__':
         print(response)
         if(response[0:5] == "CNCO2"):
                 print("\nFound Heater at " + port + "\n")
+                heater.reset_input_buffer()
+                heater.write(b'start 45')
+                time.sleep(1)
+                response = heater.readline().decode('utf-8').strip()
+
                 while True:
-                        heater.reset_input_buffer()
-                        heater.write(b'start')
-                        time.sleep(1)
                         response = heater.readline().decode('utf-8').strip()
                         if(response != ""):
-                                print("\nResponse:")
-                                print("\n"+response+"\n")
+                                print(response)
                         else:
                                 print(".")
         else:
